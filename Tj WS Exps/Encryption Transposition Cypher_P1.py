335bd5419c0b1211b433fe3445727aae958bd831
Tarun Tanmay <tarun.tanmay2510@gmail.com>
2021-05-03 02:49:15 +0530

    Add files via upload

---
+++ Tj WS Exps/Encryption Transposition Cypher_P1.py
@@ -0,0 +1,40 @@
+def encrypt(message, keyword):
+  matrix = createEncMatrix(len(keyword), message)
+  keywordSequence = getKeywordSequence(keyword)
+  ciphertext = "";
+  for num in range(len(keywordSequence)):
+    pos = keywordSequence.index(num+1)
+    for row in range(len(matrix)):
+      if len(matrix[row]) > pos:
+        ciphertext += matrix[row][pos]
+  return ciphertext
+
+def createEncMatrix(width, message):
+  r = 0
+  c = 0
+  matrix = [[]]
+  for pos, ch in enumerate(message):
+    matrix[r].append(ch)
+    c += 1
+    if c >= width:
+      c = 0
+      r += 1
+      matrix.append([])
+  return matrix
+
+def getKeywordSequence(keyword):
+  sequence = []
+  for pos, ch in enumerate(keyword):
+    previousLetters = keyword[:pos]
+    newNumber = 1
+    for previousPos, previousCh in enumerate(previousLetters):
+      if previousCh > ch:
+        sequence[previousPos] += 1
+      else:
+        newNumber += 1
+    sequence.append(newNumber)
+  return sequence
+
+msg = input("Enter the Message: ")
+key = input("Enter the Keyword: ")
+print("The Encrypted Message is: ", encrypt(msg, key))
