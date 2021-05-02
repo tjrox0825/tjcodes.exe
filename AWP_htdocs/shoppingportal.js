3f8bf504cb72802161bb0870b70f9bb412a072e5
Tarun Tanmay <tarun.tanmay2510@gmail.com>
2021-05-03 01:59:06 +0530

    Add files via upload

---
+++ AWP_htdocs/shoppingportal.js
@@ -0,0 +1,26 @@
+// Sticky menu background Transparency
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 150) {
+        this.document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+        this.document.querySelector('#navbar').style.opacity = 1;
+    }
+}
+);
+
+//Smooth Scrolling 
+$('#navbar a,.btn').on('click', function (event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 10
+            },
+            800
+        );
+
+    }
+});
